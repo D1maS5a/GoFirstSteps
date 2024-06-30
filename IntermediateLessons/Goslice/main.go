86092e406a012c7af99581ceb2ce623000dd579a
Dima Poluda <derossi@list.ru>
2024-07-01 00:04:03 +0300

    End of slice

--- IntermediateLessons/Goslice/main.go
+++ IntermediateLessons/Goslice/main.go
@@ -50,6 +50,11 @@ func main() {
 		fmt.Println(list2)
 	*/
 
+	// Свой append
+	list := make([]int, 4, 5)
+	list = Append(list, 1)
+
+	fmt.Println(list, len(list), cap(list))
 }
 
 func double(nums []int) []int {
@@ -101,5 +106,24 @@ func FindDigits(filename string) []byte {
 	copy(res, b)
 	//append
 
+	return res
+}
+
+func Append(list []int, el int) []int {
+	var res []int
+
+	resLen := len(list) + 1
+
+	if resLen <= cap(list) {
+		res = list[:resLen]
+	} else {
+		resCap := resLen
+		if resCap < 3*len(list) {
+			resCap = 3 * len(list)
+		}
+		res = make([]int, resLen, resCap)
+		copy(res, list)
+	}
+	res[len(list)] = el
 	return res
 }
