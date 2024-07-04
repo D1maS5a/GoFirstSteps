9f0df7bd03b586b97535c0992a177b487cb4e10f
Dima Poluda <derossi@list.ru>
2024-07-04 20:56:41 +0300

    const iota add

--- IntermediateLessons/UnknownTips/main.go
+++ IntermediateLessons/UnknownTips/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	x := 2
@@ -59,4 +61,77 @@ func main() {
 	part2 := []int{4, 5, 6}
 	combined := append(part1, part2...)
 	fmt.Println(combined)
+
+	if j := 5; j < 10 {
+		//..
+	} else {
+		//..
+	}
+
+	str := "user"
+	if rank := getUserRank(str); rank == "admin" {
+		//..
+	} else if rank == "manager" {
+		//..
+	} else {
+		//..
+	}
+
+	const (
+		Online      = iota // 0 iota == serial
+		Offline            // 1
+		Maintenance        // 2
+		Retired            // 3
+	)
+
+	// пропуск значений
+	const (
+		s0 = iota // 0
+		_         // 1
+		_         // 2
+		s3        // 3
+	)
+
+	// начать с 3
+	const (
+		i3 = iota + 3 // 3
+		i4            // 4
+		i5            // 5
+	)
+
+	fmt.Println(North)
+	north := North
+	fmt.Println(north)
+}
+
+func getUserRank(rank string) string {
+	return rank
+}
+
+const (
+	North = iota
+	East
+	South
+	West
+)
+
+type Direction byte
+
+func (d Direction) Dir() string {
+	switch d {
+	case North:
+		return fmt.Sprintf("North")
+	case South:
+		return fmt.Sprintf("South")
+	case East:
+		return fmt.Sprintf("East")
+	case West:
+		return fmt.Sprintf("West")
+	default:
+		return "other direction"
+	}
+}
+
+func (d Direction) String() string {
+	return []string{"North", "East", "South", "West"}[d]
 }
