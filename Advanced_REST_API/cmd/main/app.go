48c3d892d954afc57285c2ba6bca845545a2ee21
D1maS5a <derossi@list.ru>
2024-07-30 15:02:49 +0300

    end of config 3

--- Advanced_REST_API/cmd/main/app.go
+++ Advanced_REST_API/cmd/main/app.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"advanREST/internal/config"
 	"advanREST/internal/user"
 	"advanREST/pkg/logging"
+	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
@@ -14,32 +19,56 @@ import (
 //		name := params.ByName("name")
 //		w.Write([]byte(fmt.Sprintf("Hello %s", name)))
 //	}
-//
-// 14:30
+
 func main() {
 	logger := logging.GetLogger()
 	// log.Println("create router")
 	logger.Info("create router")
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
+
 	// log.Println("register user handler")
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	// router.HandlerFunc("GET", "/", IndexHandler)
 	// router.GET("/:name", IndexHandler)
 
 	logger := logging.GetLogger()
 	// log.Println("start application")
 	logger.Info("start application")
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		panic(err)
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		// /path/to/binary
+		// Dir() -- /path/to
+		logger.Info("detect app port")
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+
+		logger.Info("listen unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server is listening unix socket %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+		logger.Infof("server is listening port %s:%s", cfg.Listen.BindIP, cfg.Listen.Port)
+	}
+
+	if listenErr != nil {
+		logger.Fatal(listenErr)
 	}
 
 	server := &http.Server{
@@ -49,7 +78,7 @@ func start(router *httprouter.Router) {
 	}
 
 	// log.Println("server is listening port 0.0.0.0:1234")
-	logger.Info("server is listening port 0.0.0.0:1234")
+
 	// log.Fatalln(server.Serve(listener))
 	logger.Fatal(server.Serve(listener))
 }
